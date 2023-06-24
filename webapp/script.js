93fd385a5ac70a23c387b28622ae758745d27c8c
Fabian Klötzl <fabian@kloetzl.info>
2023-06-24 12:22:44 +0100

    sort articles lexicographically

--- webapp/script.js
+++ webapp/script.js
@@ -1,29 +1,37 @@
 const entryList = document.getElementById('entry-list');
 const filterInput = document.getElementById('filter-input');
 
-function loadMarkdownFiles() {
+async function loadMarkdownFiles() {
   const converter = new markdownit();
-  mdFiles.forEach((file) => {
-    fetch(file)
-      .then((response) => response.text())
-      .then((markdown) => {
-        if (!markdown) return;
-        const html = converter.render(markdown);
-        const listItem = document.createElement('li');
-        listItem.innerHTML = html;
-        listItem.classList.add('list-group-item');
-        entryList.appendChild(listItem);
-      })
-      .catch((error) => {
-        console.error('Error loading markdown file:', error);
-      });
+  const contents = mdFiles.map(async function(path) {
+    const response = await fetch(path);
+    const markdown = await response.text();
+    const html = converter.render(markdown);
+    const listItem = document.createElement('li');
+    listItem.innerHTML = html;
+    listItem.classList.add('list-group-item');
+    return listItem;
   });
+
+  var elements = await Promise.all(contents);
+  elements.sort(function (a, b) {
+    const aa = a.querySelectorAll('h1, h2')[0].innerText;
+    const bb = b.querySelectorAll('h1, h2')[0].innerText;
+    if (aa === bb) return 0;
+    return aa < bb ? -1 : 1;
+  });
+
+  const wrapper = document.createDocumentFragment();
+  for (var listItem of elements) {
+    wrapper.appendChild(listItem);
+  }
+  entryList.appendChild(wrapper);
 }
 
 function filterEntries() {
   const filterValue = filterInput.value.toLowerCase();
   const entries = entryList.getElementsByTagName('li');
-  const searchType = document.querySelector('input[name="search-type"]:checked').value;
+  const searchType = 'simple'; //document.querySelector('input[name="search-type"]:checked').value;
 
   Array.from(entries).forEach((entry) => {
     const text = entry.innerText.toLowerCase();
